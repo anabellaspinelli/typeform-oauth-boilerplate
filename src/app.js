48ae9846890eb5445572ab7f3314911a3db639a5
Anabella Spinelli <anabella.spinelli@typeform.com>
2018-05-24 12:39:56 +0200

    Added pretty bokeh image to homepage.

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.use(
   cookieSession({
